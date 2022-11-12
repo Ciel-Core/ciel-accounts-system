fc69e518150452521f0b3549df19838d38ea9196
0xENDER <72353925+0xENDER@users.noreply.github.com>
2022-11-12 23:56:22 +0200

    Update `GlobalBar`

--- src/assets/components/GlobalBar.jsx
+++ src/assets/components/GlobalBar.jsx
@@ -5,8 +5,13 @@
  **/
 
 import { createSignal } from "solid-js";
+import { isSignedIn, signOut } from "./../scripts/user.jsx";
 import styles from './../styles/globalBar.module.css';
-import LoadingSpinner from './LoadingSpinner.jsx'
+import LoadingSpinner from './LoadingSpinner.jsx';
+
+import SignOutIcon from './../icons/sign_out.svg';
+import BellIcon from './../icons/bell.svg';
+import BellPinIcon from './../icons/bell_pin.svg';
 
 function UserProfile(props){
     const [showImage, setShowImage] = createSignal(false);
@@ -21,15 +26,20 @@ function UserProfile(props){
 function LeftControls(props){
     return (
         <div class={styles.leftControls}>
-            {0}
+            <div class={styles.navControl}>Back</div>
+            <div style={{display: (isSignedIn()) ? "block" : "none"}}>Other</div>
         </div>
     );
 }
 
 function RightControls(props){
     return (
-        <div class={styles.rightControls}>
-            {1}
+        <div class={styles.rightControls} style={{display: (isSignedIn()) ? "block" : "none"}}>
+            <div class={styles.bellContainer} data-pin={false}>
+                <BellIcon class={styles.bell}/>
+                <BellPinIcon class={styles.bellPin}/>
+            </div>
+            <SignOutIcon onClick={signOut}/>
         </div>
     );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,9 +4,12 @@
  * 
  **/
 
+import style from './../assets/styles/pages/home.module.css';
+
 import { Title } from './../assets/components/Title.jsx';
 import { onCleanup, onMount } from 'solid-js';
-import { signOut } from './../assets/scripts/user.jsx';
+import { signOut, userData } from './../assets/scripts/user.jsx';
+import { Button } from './../assets/components/CustomElements.jsx';
 
 export default function Home(props){
     onCleanup(() => {
@@ -17,7 +20,8 @@ export default function Home(props){
     });
     return <>
         <Title>Home</Title>
-        <button onClick={signOut}>Sign Out</button>
-        "/"!
+        <h2 class={style.userWelcome}>Welcome, {userData().displayUsername}!</h2>
+        <h4 class={style.userMessage}>Manage your profile, privacy preferences, and security across all connected services.</h4>
+        <Button type={"action"} function={signOut} style={{margin: "auto"}}>Sign out</Button>
     </>;
 }
