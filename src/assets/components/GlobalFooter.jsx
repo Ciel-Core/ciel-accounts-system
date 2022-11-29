7d8efd185d070b78dff0ff751b3424bac403fda1
0xENDER <72353925+0xENDER@users.noreply.github.com>
2022-11-29 17:36:11 +0200

    Add more info in the `about` dialog

--- src/assets/components/GlobalFooter.jsx
+++ src/assets/components/GlobalFooter.jsx
@@ -5,6 +5,7 @@
  **/
 
 import styles from './../styles/globalFooter.module.css';
+import generalStyles from './../styles/general.module.css';
 
 import InfoIcon from './../icons/info.svg';
 import { useNavigate } from '@solidjs/router';
@@ -23,9 +24,36 @@ function GlobalFooter(props){
                 unselectable>
             <FooterLink href={"/about/legal"} disable={!isOnline()}>Legal</FooterLink>
             <FooterLink href={"/about/services"} disable={!isOnline()}>Services</FooterLink>
-            <InfoIcon class={styles.icon} onClick={() => showDialog(`About ${import.meta.env.VITE_NAME}`, (<FlexContainer horizontal>
-                <h4>Version 0.00.00 (beta)</h4>
-                <a>LICENSES</a>
+            <InfoIcon class={styles.icon} onClick={() => showDialog(`About ${import.meta.env.VITE_NAME}`, (<FlexContainer class={styles.aboutContainer} horizontal>
+                <h4>
+                    Version {
+                        import.meta.env.VITE_V_FRONT_MAJOR}.
+                        {import.meta.env.VITE_V_FRONT_MINOR}.
+                        {import.meta.env.VITE_V_FRONT_FIX
+                    } ({import.meta.env.VITE_V_TYPE})
+                </h4>
+                <h5>
+                    Server version {
+                        import.meta.env.VITE_V_BACK_MAJOR}.
+                        {import.meta.env.VITE_V_BACK_FIX
+                    }_PHP_MYSQL
+                </h5>
+                <h5>
+                    No copyright applys.
+                    No trademarks.
+                    Owned by <a
+                        href={import.meta.env.VITE_OWNER_LINK} target={"_blank"}
+                        class={generalStyles.link}>
+                            {import.meta.env.VITE_OWNER_NAME}
+                        </a>
+                </h5>
+                <h5>
+                    Source code available on <a
+                        href={import.meta.env.VITE_GITHUB_REPOSITORY_LINK} target={"_blank"}
+                        class={generalStyles.link}>
+                            GitHub
+                        </a>
+                </h5>
             </FlexContainer>))}/>
         </div>
     );
