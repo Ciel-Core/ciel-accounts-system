62cd1d3a76b5b7fd34c0d0c6cef322d3174ec6d5
0xENDER <72353925+0xENDER@users.noreply.github.com>
2022-11-29 16:17:11 +0200

    Fix a bug

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -4,7 +4,7 @@
  * 
  **/
 
-import { For, onMount } from 'solid-js';
+import { For, onCleanup, onMount } from 'solid-js';
 import { afterURLChange } from './../scripts/traffic.jsx';
 import generalStyles from './../styles/general.module.css';
 
@@ -12,7 +12,7 @@ import Link from './Link.jsx';
 import { processProps } from './_custom.jsx';
 
 function updateShadows(content, start, end){
-    if(content.scrollWidth == content.scrollLeft + content.clientWidth){
+    if(content.scrollLeft + content.clientWidth >= content.scrollWidth){
         end.style.display = "none";
         start.style.display = null;
     }else if(content.scrollLeft == 0){
@@ -31,50 +31,67 @@ export function NavBar(props){
         linksElms = [],
         isHome = false,
         selected = false,
+        scrollToOption = function(link){
+            content.scrollTo({
+                top: 0,
+                left: (function(){
+                    let s = 0,
+                        lastW = 0,
+                        stop = false;
+                    linksElms.forEach(l => {
+                        if(!stop){
+                            if(l[1] == link[1]){
+                                stop = true;
+                                s -= (link[1] == linksElms[linksElms.length - 1][1]) ? 0 : lastW;
+                            }else{
+                                s += l[2].clientWidth;
+                                lastW = l[2].clientWidth;
+                            }
+                        }
+                    });
+                    return s;
+                })(),
+                behavior: (firstScroll) ? (firstScroll = false, "auto") : "smooth"
+            });
+        },
         callback = function(){
             isHome = location.pathname.replace(/[#?].*$/g, "") == "/";
             linksElms.forEach(link => {
                 if((!selected && isHome) ||
                     (!selected && location.pathname.indexOf(link[1]) == 0 && link[1] != props.links[0][1])){
                     link[2].setAttribute("selected", "");
-                    content.scrollTo({
-                        top: 0,
-                        left: (function(){
-                            let s = 0,
-                                lastW = 0,
-                                stop = false;
-                            linksElms.forEach(l => {
-                                if(!stop){
-                                    if(l[1] == link[1]){
-                                        stop = true;
-                                        s -= (link[1] == linksElms[linksElms.length - 1][1]) ? 0 : lastW;
-                                    }else{
-                                        s += l[2].clientWidth;
-                                        lastW = l[2].clientWidth;
-                                    }
-                                }
-                            });
-                            return s;
-                        })(),
-                        behavior: (firstScroll) ? (firstScroll = false, "auto") : "smooth"
-                    });
+                    scrollToOption(link);
                     selected = true;
                 }
             });
         },
         content,
-        startShadow, endShadow;
+        startShadow, endShadow,
+        resizeUpdate = function(){
+            linksElms.forEach(link => {
+                if(link[2].hasAttribute("selected")){
+                    scrollToOption(link);
+                }
+            });
+        };
     onMount(() => {
         afterURLChange(function(){
             selected = false;
             linksElms.forEach(l => l[2].removeAttribute("selected"));
             callback();
-            updateShadows(content, startShadow, endShadow);
         });
         setTimeout(() => {
             callback();
             updateShadows(content, startShadow, endShadow);
         }, 1);
+        window.addEventListener("resize", resizeUpdate);
+        content.onscroll = function(){
+            updateShadows(content, startShadow, endShadow);
+        };
+    });
+    onCleanup(() => {
+        updateShadows(content, startShadow, endShadow);
+        window.removeEventListener("resize", resizeUpdate);
     });
     return (<div class={basicProps.class} style={basicProps.style} unselectable>
         <div ref={startShadow} class={generalStyles.startShadow} style={{display: "none"}}></div>
