cb348ea105ec5a07c89802738003162551bcbb8e
0xENDER <72353925+0xENDER@users.noreply.github.com>
2022-12-01 15:11:14 +0200

    Move `admin` dir

--- public/admin/tools/webauthn.generate.certificates.php
+++ public/admin/tools/webauthn.generate.certificates.php
@@ -3,8 +3,8 @@
 // Use this file to generate FIDO Alliance certificates for WebAuthn
 // Generate new certificates once every 1-2 months
 
-require_once './../tools/server.info.php';
-require_once './../tools/tool.web.authn.php';
+require_once './../../APIs/tools/server.info.php';
+require_once './../../APIs/tools/tool.web.authn.php';
 
 global $WebAuthn;
 $WebAuthn->queryFidoMetaDataService("$SERVER_ROOT/certificates/fido/", true);
