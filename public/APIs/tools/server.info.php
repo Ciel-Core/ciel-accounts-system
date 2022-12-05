82e0a8e64bafbcfd4daf0767890d15873a270952
0xENDER <72353925+0xENDER@users.noreply.github.com>
2022-12-05 13:56:11 +0200

    Update server.info.php

--- public/APIs/tools/server.info.php
+++ public/APIs/tools/server.info.php
@@ -6,4 +6,8 @@ $SERVER_PROTOCOL = stripos($_SERVER['SERVER_PROTOCOL'],'https') === 0 ? 'https:/
 // Get the server's root
 $SERVER_ROOT = $_SERVER['DOCUMENT_ROOT'];
 
+global $G_SERVER_ROOT, $G_SERVER_PROTOCOL;
+$G_SERVER_ROOT              = $SERVER_ROOT;
+$G_SERVER_PROTOCOL          = $SERVER_PROTOCOL;
+
 ?>
