62dd70e58143f13916f1fe6be71217b53b30d8ca
0xENDER <72353925+0xENDER@users.noreply.github.com>
2022-12-26 00:13:01 +0200

    Add a description

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,49 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import solidSvg from "vite-plugin-solid-svg";
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig({
-    plugins: [
-        solidPlugin(),
-        solidSvg(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            workbox: {
-                globPatterns: ['**/*.{js,css,html}', 'images/**/*.{svg,png,ico}'],
-                cleanupOutdatedCaches: true,
-                maximumFileSizeToCacheInBytes: (12 /*MiB*/ ) * 1000000
-            },
-            devOptions: {
-                enabled: true
-            },
-            manifest: {
-                name: 'MyPWA',
-                short_name: 'aPWA',
-                description: 'My PWA for you!',
-                theme_color: '#ffffff',
-                icons: [{
-                        src: '/images/icon-192.png',
-                        sizes: '192x192',
-                        type: 'image/png'
-                    },
-                    {
-                        src: '/images/icon-512.png',
-                        sizes: '512x512',
-                        type: 'image/png'
-                    }
-                ]
-            }
-        })
-    ],
-    server: {
-        port: 80
-    },
-    build: {
-        minify: true,
-        target: 'esnext'
-    }
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [
+            solidPlugin(),
+            solidSvg(),
+            VitePWA({
+                registerType: 'autoUpdate',
+                workbox: {
+                    globPatterns: ['**/*.{js,css,html}', 'images/**/*.{svg,png,ico}'],
+                    cleanupOutdatedCaches: true,
+                    maximumFileSizeToCacheInBytes: (12 /*MiB*/ ) * 1000000
+                },
+                devOptions: {
+                    enabled: true
+                },
+                manifest: {
+                    name: env.VITE_NAME,
+                    short_name: env.VITE_NAME,
+                    description: env.VITE_DESCRIPTION,
+                    theme_color: '#ffffff',
+                    icons: [{
+                            src: '/images/icon-192.png',
+                            sizes: '192x192',
+                            type: 'image/png'
+                        },
+                        {
+                            src: '/images/icon-512.png',
+                            sizes: '512x512',
+                            type: 'image/png'
+                        }
+                    ]
+                }
+            })
+        ],
+        server: {
+            port: 80
+        },
+        build: {
+            minify: true,
+            target: 'esnext'
+        }
+    };
 });
