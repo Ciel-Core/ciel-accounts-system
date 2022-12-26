9eb388e64a143b540b654cdd01512621aba7012f
0xENDER <72353925+0xENDER@users.noreply.github.com>
2022-12-26 12:20:40 +0200

    Add console logs history

--- src/assets/scripts/console.jsx
+++ src/assets/scripts/console.jsx
@@ -52,11 +52,11 @@ export function detectDevTools(callback) {
                 if(!userWarned){
                     if(!noConsoleLog){
                         console.clear();
-                        console.log('%cStop!',
+                        console.log(IGNORE_LOG, '%cStop!',
                                         'color: crimson; font-size: 46px; font-weight: 800;');
-                        console.log('%cDo NOT paste any code or text into the console!',
+                        console.log(IGNORE_LOG, '%cDo NOT paste any code or text into the console!',
                                         'color: orange; font-size: 24px;');
-                        console.log("%cYour account, data, and privacy could be compromised " +
+                        console.log(IGNORE_LOG, "%cYour account, data, and privacy could be compromised " +
                                         "should you allow other people to access your console " +
                                         "or paste code/text into it!", 'font-size: 16px;');
                     }
